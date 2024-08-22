93702a69ea293cacb22fa3dc0b6d365d00776cc0
Fil <filongunio@gmail.com>
2024-08-23 02:37:01 +0800

    adjust frontend of page for clients pov

--- resources/views/livewire/request.blade.php
+++ resources/views/livewire/request.blade.php
@@ -1,34 +1,46 @@
 <?php
 
 use Livewire\Volt\Component;
+use App\Models\User;
+use App\Models\PersonInContact;
 
 new class extends Component {
     public $clientTypes = ['all', 'business', 'political'];
     public $search = '';
+
+    public function with(): array {
+        $personInContacts = PersonInContact::where('user_id', auth()->user()->id);
+        return [
+            'personInContacts' => (clone $personInContacts)->get(),
+            'personInContact' => (clone $personInContacts)->first(),
+    ];
+    }
 }; ?>
 
-<div class="flex flex-col items-center justify-center w-full">
-    <div class="flex flex-col gap-20 md:ml-24 md:flex-row">
-        <div class="flex flex-1 flex-col gap-10 px-8 py-8 border rounded-3xl md:flex-row md:w-[47rem]">
-            <img class="w-auto h-[15rem] md:h-[12rem]" src="{{ asset('images/user.png') }}" alt="">
+<div class="flex flex-col items-center justify-center w-full mx-auto">
+    <div class="flex flex-col items-stretch justify-center max-w-screen-xl gap-10 lg:flex-row">
+        <div class="flex flex-col items-center justify-center flex-grow gap-10 px-8 py-8 border sm:flex-row rounded-3xl">
+            <img class="w-auto max-h-[12rem]" src="{{ asset('images/user.png') }}" alt="">
             <div class="flex flex-col justify-center ">
-                <h1 class=" text-4xl font-bold mb-2">Client Name A</h1>
-                <h2 class=" text-lg">Email Address:</h2>
-                <h2 class=" text-lg">Company Cell Number:</h2>
-                <h2 class=" text-lg mb-6">Company Address:</h2>
-                <h2 class=" text-lg">Porject Manager: Jubie</h2>
+                <h1 class="mb-2 text-4xl font-bold ">{{ auth()->user()->name }}</h1>
+                <h2 class="text-lg">Email: <span class="text-gray-500">{{ auth()->user()->email }}</span></h2>
+                <h2 class="text-lg">Company Cell Number: <span class="text-gray-500">{{ auth()->user()->company_cell_number }}</span></h2>
+                <h2 class="text-lg">Company Address: <span class="text-gray-500">{{ auth()->user()->company_address }}</span></h2>
+                <h2 class="text-lg">Porject Manager: <span class="text-gray-500">{{ auth()->user()->project_manager }}</span></h2>
             </div>
         </div>
-        <div class=" flex flex-1 flex-col justify-center ">
-            <h1 class=" text-4xl font-bold mb-2">Person in Contact:</h1>
-            <h2 class=" text-lg">Name:</h2>
-            <h2 class=" text-lg">Email Address:</h2>
-            <h2 class=" text-lg mb-6">Cell Number:</h2>
-            <button class=" py-1 w-28 text-lg tracking-wide text-black transition-all duration-300 ease-in-out rounded-xl bg-white hover:opacity-60">View More</button>
+        <div class="flex flex-col justify-center">
+            <h1 class="mb-2 text-4xl font-bold ">Person in Contact</h1>
+            <h2 class="text-lg">Name: <span class="text-gray-500">{{ $personInContact?->name }}</span></h2>
+            <h2 class="text-lg">Email Address: <span class="text-gray-500">{{ $personInContact?->email }}</span></h2>
+            <h2 class="mb-6 text-lg">Cell Number: <span class="text-gray-500">{{ $personInContact?->cell_number }}</span></h2>
+            @if ($personInContacts->count() > 1)
+            <button class="py-1 text-black transition-all duration-300 ease-in-out bg-white text-lgtracking-wide w-28 rounded-xl hover:opacity-60">View More</button>
+            @endif
         </div>
     </div>
     <div class="bg-custom-gradient w-full h-[2px] -z-10 my-10"></div>
-    <div class="w-full p-3 text-black bg-white rounded-lg mb-16 lg:p-6">
+    <div class="w-full p-3 mb-16 text-black bg-white rounded-lg lg:p-6">
         <h1 class="font-bold lg:text-3xl">Client Requests</h1>
         <table class="w-full mt-5 border-collapse">
             <thead>
--- resources/views/request.blade.php
+++ resources/views/request.blade.php
@@ -1,7 +1,7 @@
 <x-admin-layout>
     <div class="flex flex-col items-center justify-start gap-5">
-        <div class="w-full flex justify-center">
-            <h1 class="text-3xl font-bold mb-2 md:text-6xl md:mb-8 lg:text-8xl ">Welcome to your Portal!</h1>
+        <div class="flex justify-center w-full">
+            <h1 class="mb-2 text-3xl font-bold text-center md:text-6xl md:mb-8 lg:text-left">Welcome to your Portal!</h1>
         </div>
         @livewire('request')
     </div>
