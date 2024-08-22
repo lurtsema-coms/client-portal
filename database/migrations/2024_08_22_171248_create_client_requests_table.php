58bdbf49d9f6c0a681912b308e437439ba8e379a
Fil <filongunio@gmail.com>
2024-08-23 03:45:27 +0800

    add softdeletes to client request migration

--- database/migrations/2024_08_22_171248_create_client_requests_table.php
+++ database/migrations/2024_08_22_171248_create_client_requests_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->text('remarks');
             $table->integer('updated_by')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
