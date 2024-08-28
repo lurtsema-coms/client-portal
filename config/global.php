6ee6c12f849c0f7b6f0363b4b307bf7028c6f058
Fil <filongunio@gmail.com>
2024-08-29 00:25:48 +0800

    create global config file

---
+++ config/global.php
@@ -0,0 +1,12 @@
+<?php
+return [
+  'deliverable_statuses' => [
+    'REQUESTED',
+    'PENDING',
+    'IN-PROGRESS',
+    'COMPLETED',
+    'CANCELLED',
+    'REJECTED',
+    'REJECTED-BY-CLIENT',
+  ],
+];
