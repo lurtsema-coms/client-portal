8f394c0e909ba2a9ae663611250e9cc3bddee6ec
Fil <filongunio@gmail.com>
2024-08-29 00:49:56 +0800

    bring back the view request blade

---
+++ resources/views/livewire/requests/view-request.blade.php
@@ -0,0 +1,12 @@
+<?php
+use Livewire\Volt\Component;
+use Livewire\Attributes\Layout;
+
+new #[Layout('layouts.admin')] 
+class extends Component {
+  //
+}; ?>
+
+<div>
+  hello from the request view
+</div>
