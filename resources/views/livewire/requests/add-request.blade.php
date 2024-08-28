53a4d4dc69a4f2ee5dce55a4ee7b4013b747c938
Patwicku <punzalan2233@gmail.com>
2024-08-29 02:54:06 +0800

    trix rich editor and add client request

--- resources/views/livewire/requests/add-request.blade.php
+++ resources/views/livewire/requests/add-request.blade.php
@@ -5,66 +5,33 @@ use Livewire\Attributes\Validate;
 use Livewire\Volt\Component;
 use Livewire\Attributes\Layout;
 
-
 new #[Layout('layouts.admin')] 
 class extends Component {
 
-    
+    #[Validate('required')] 
     public $title = '';
-    public $needed_at = '';
+    #[Validate('required')] 
+    public $date_need = '';
+    #[Validate('required')] 
     public $remarks = '';
-
-
-    public function mount()
-    {
-        $this->request_info = [
-            [
-                'title' => '',
-                'needed_at' => '',
-                'remarks' => '',
-            ]
-        ];
-
-    }
 
     public function addRequest()
     {
+        $this->validate();
 
         $user_id = ClientRequest::insertGetId([
             'title' => $this->title,
             'status' => 'PENDING',
             'user_id' => auth()->user()->id,
-            'needed_at' => $this->needed_at,
+            'needed_at' => $this->date_need,
             'remarks' => $this->remarks,
             'created_at' => date('Y-m-d H:i:s')
         ]);
-        
 
-
-        $this->reset(['title', 'needed_at', 'remarks']);
+        $this->reset(['title', 'date_need', 'remarks']);
         
         session()->flash('status', 'Request Successfully added');
         $this->redirect('/request', navigate: true);
-    }
-
-    public function rules()
-    {
-        $rules = [
-            'title' => 'required',
-            'needed_at' => 'required',
-            'remarks' => 'required',
-        ];
-
-        return $rules;
-    }
-
-    public function messages()
-    {
-        return [
-            'request_info.*.title.required' => 'The name field is required.',
-            'request_info.*.needed_at.required' => 'The needed date field is required.',
-            'request_info.*.remarks.required' => 'The remarks field is required.',
-        ];
     }
 
 }; ?>
@@ -92,20 +59,23 @@ class extends Component {
                     <input 
                         class="w-full text-black rounded-lg"
                         type="date"
-                        wire:model="needed_at"
+                        wire:model="date_need"
                     >
-                    @error('needed_at')<p class="text-red-500">{{ $message }}</p>@enderror
-                </div>
-                <div class="mt-5 space-y-2">
-                    <label for="" class="block tracking-wider text-gray-600">Remarks</label>
-                    <textarea 
-                        id="remarks"
-                        class="w-full text-black rounded-lg"
-                        wire:model="remarks"
-                    ></textarea>
-                    @error('remarks')<p class="text-red-500">{{ $message }}</p>@enderror
+                    @error('date_need')<p class="text-red-500">{{ $message }}</p>@enderror
                 </div>
             </div>
+            <div class="w-full mt-5 space-y-2" wire:ignore>
+                    <label for="" class="block tracking-wider text-gray-600">Remarks</label>
+                    <trix-editor
+                        class="trix"
+                        x-data
+                        x-on:trix-change="$dispatch('input', event.target.value)"
+                        x-ref="trix"
+                        wire:model.debounce.60s="remarks"
+                        wire:key="uniqueKey"
+                    ></trix-editor>
+            </div>
+            @error('remarks')<p class="mt-2 text-red-500">{{ $message }}</p>@enderror
             <button 
                 class="float-right px-4 py-2 mt-5 text-right text-white bg-blue-500 border rounded-lg hover:bg-blue-600"
                 type="Submit" 
@@ -115,4 +85,3 @@ class extends Component {
         </form>
     </div>
 </div>
-
