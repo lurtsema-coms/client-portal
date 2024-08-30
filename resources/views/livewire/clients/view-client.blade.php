e8687c28929805baab010deb854f988e3a96cec9
Fil <filongunio@gmail.com>
2024-08-31 00:50:23 +0800

    refactor code for editing request and deliverable on admin side

--- resources/views/livewire/clients/view-client.blade.php
+++ resources/views/livewire/clients/view-client.blade.php
@@ -1,47 +1,233 @@
 <?php
 
-use Livewire\Volt\Component;
-use App\Models\PersonInContact;
 use App\Models\User;
+use App\Models\ClientRequest;
+use App\Models\PersonInContact;
+use Livewire\Attributes\Url;
+use Livewire\WithPagination;
+use Livewire\Volt\Component;
 use Livewire\Attributes\Layout;
 
-
-new #[Layout('layouts.admin')] 
+new #[Layout('layouts.admin')]
 class extends Component {
-    public $client;
-    public $personInContacts;
-    public $personInContact;
 
-    public function mount(User $client) {
+    use WithPagination;
+    
+    #[Url]
+    public $search;
+    public $person_in_contact;
+    public $client;
+
+    public function mount(User $client)
+    {
         $this->client = $client;
-        $personInContacts = PersonInContact::where('user_id', auth()->user()->id);
-        $this->personInContacts = (clone $personInContacts)->get();
-        $this->personInContact = (clone $personInContacts)->first();
+        $user = User::with('personInContact')->find($client->id);
+        $this->person_in_contact = $user->personInContact;
+    }
+
+    public function with(): array 
+    {
+        $personInContacts = PersonInContact::where('user_id', $this->client->id);
+        $userRequests = ClientRequest::with('user')
+            ->where('user_id', $this->client->id)
+            ->where('status', 'PENDING')
+            ->when($this->search, function ($query) {
+                $query->where(function ($query) {
+                    $query->where('title', 'like', '%' . $this->search . '%')
+                        ->orWhere('remarks', 'like', '%' . $this->search . '%')
+                        ->orWhereRaw("DATE_FORMAT(needed_at, '%a, %M %e, %Y') LIKE ?", ['%' . $this->search . '%']);
+                });
+            })
+            ->paginate(5);
+        $deliverables = ClientRequest::with('user', 'updatedBy')
+            ->where('user_id', $this->client->id)
+            ->where('status', '!=', 'PENDING')
+            ->when($this->search, function ($query) {
+                $query->where(function ($query) {
+                    $query->where('title', 'like', '%' . $this->search . '%')
+                        ->orWhere('status', 'like', '%' . $this->search . '%')
+                        ->orWhere('remarks', 'like', '%' . $this->search . '%')
+                        ->orWhereRaw("DATE_FORMAT(created_at, '%a, %M %e, %Y') LIKE ?", ['%' . $this->search . '%'])
+                        ->orWhereRaw("DATE_FORMAT(needed_at, '%a, %M %e, %Y') LIKE ?", ['%' . $this->search . '%']);
+                });
+            })
+            ->paginate(5);
+        
+        return [
+            'personInContacts' => (clone $personInContacts)->get(),
+            'personInContact' => (clone $personInContacts)->first(),
+            'userRequests' => $userRequests,
+            'deliverables' => $deliverables,
+        ];
     }
 }; ?>
 
 <div class="flex flex-col items-center justify-center w-full mx-auto">
-    <div class="flex items-center justify-between">
-        <div class="flex flex-col items-stretch justify-center max-w-screen-xl gap-10 lg:flex-row">
-            <div class="flex flex-col items-center justify-center flex-grow gap-10 px-8 py-8 border sm:flex-row rounded-3xl">
-                <img class="w-auto max-h-[12rem]" src="{{ asset('images/user.png') }}" alt="">
-                <div class="flex flex-col justify-center ">
-                    <h1 class="mb-2 text-4xl font-bold ">{{ $client->name }}</h1>
-                    <h2 class="text-lg">Email: <span class="text-gray-500">{{ $client->email }}</span></h2>
-                    <h2 class="text-lg">Company Cell Number: <span class="text-gray-500">{{ $client->company_cell_number }}</span></h2>
-                    <h2 class="text-lg">Company Address: <span class="text-gray-500">{{ $client->company_address }}</span></h2>
-                    <h2 class="text-lg">Project Manager: <span class="text-gray-500">{{ $client->project_manager }}</span></h2>
+    <div class="flex flex-col items-stretch justify-center max-w-screen-xl gap-10 lg:flex-row">
+        <div class="flex flex-col items-center justify-center flex-grow gap-10 px-8 py-8 border sm:flex-row rounded-3xl">
+            <img class="w-auto max-h-[12rem]" src="{{ asset('images/user.png') }}" alt="">
+            <div class="flex flex-col justify-center ">
+                <h1 class="mb-2 text-4xl font-bold ">{{ $client->name }}</h1>
+                <h2 class="text-lg">Email: <span class="text-gray-500">{{ $client->email }}</span></h2>
+                <h2 class="text-lg">Company Cell Number: <span class="text-gray-500">{{ $client->company_cell_number }}</span></h2>
+                <h2 class="text-lg">Company Address: <span class="text-gray-500">{{ $client->company_address }}</span></h2>
+                <h2 class="text-lg">Project Manager: <span class="text-gray-500">{{ $client->project_manager }}</span></h2>
+            </div>
+        </div>
+        <div class="flex flex-col justify-center">
+            <h1 class="mb-2 text-4xl font-bold ">Person in Contact</h1>
+            <h2 class="text-lg">Name: <span class="text-gray-500">{{ $personInContact?->name }}</span></h2>
+            <h2 class="text-lg">Email Address: <span class="text-gray-500">{{ $personInContact?->email }}</span></h2>
+            <h2 class="mb-6 text-lg">Cell Number: <span class="text-gray-500">{{ $personInContact?->cell_number }}</span></h2>
+            @if ($personInContacts->count() > 1)
+                <div 
+                    x-data="{isOpen: false}"
+                    x-init="$watch('isOpen', value => document.body.style.overflow = value ? 'hidden' : 'auto')"
+                >
+                    <button 
+                        class="py-1 text-black transition-all duration-300 ease-in-out bg-white text-lgtracking-wide w-28 rounded-xl hover:opacity-60"
+                        @click="isOpen=true"
+                    >
+                        View More
+                    </button>
+                
+                    <div x-show="isOpen" 
+                        class="fixed inset-0 z-50 flex bg-black bg-opacity-75"
+                        x-cloak
+                    >
+                        <!-- Close Button -->
+                        <button @click="isOpen=false" 
+                                class="absolute text-xl font-bold text-white top-5 right-5">
+                            &times;
+                        </button>
+                        
+                        <!-- Zoomed Image -->
+                        <div class="flex w-full p-5">
+                            <div 
+                                class="w-full max-w-4xl p-10 m-auto text-black bg-white shadow-lg rounded-2xl"
+                                @click.outside="isOpen=false"
+                            >
+                                <div class="space-y-5 overflow-auto max-h-96">
+                                    @if ($person_in_contact)
+                                        @foreach ($person_in_contact as $index => $contact)
+                                            <div wire:key="contact-users-{{ $index }}">
+                                                <h1 class="mb-4 font-bold lg:text-xl">Person in Contact {{ $index !== 0 ? $index : '' }}</h1>
+                                                <div>
+                                                    <p>
+                                                        <span class="text-gray-600">Name:</span> {{ $contact->name }}
+                                                    </p>
+                                                    <p>
+                                                        <span class="text-gray-600">Cell Number:</span> {{ $contact->cell_number }}
+                                                    </p>
+                                                    <p>
+                                                        <span class="text-gray-600">Email:</span> {{ $contact->email }}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        @endforeach
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
                 </div>
-            </div>
-            <div class="flex flex-col justify-center">
-                <h1 class="mb-2 text-4xl font-bold ">Person in Contact</h1>
-                <h2 class="text-lg">Name: <span class="text-gray-500">{{ $personInContact?->name }}</span></h2>
-                <h2 class="text-lg">Email Address: <span class="text-gray-500">{{ $personInContact?->email }}</span></h2>
-                <h2 class="mb-6 text-lg">Cell Number: <span class="text-gray-500">{{ $personInContact?->cell_number }}</span></h2>
-                @if ($personInContacts->count() > 1)
-                <button class="py-1 text-black transition-all duration-300 ease-in-out bg-white text-lgtracking-wide w-28 rounded-xl hover:opacity-60">View More</button>
-                @endif
-            </div>
-        </div>    
+            @endif
+        </div>
     </div>
+    <div class="bg-custom-gradient w-full h-[2px] -z-10 my-10"></div>
+    <div class="flex flex-col-reverse justify-center w-full gap-5 md:flex-row">
+        <div class="relative md:w-[25rem]">
+            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+                <svg class="w-5 h-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M8.5 2a6.5 6.5 0 104.5 11.29l3.85 3.85a.75.75 0 001.06-1.06l-3.85-3.85A6.5 6.5 0 008.5 2zm-5 6.5a5 5 0 1110 0 5 5 0 01-10 0z" clip-rule="evenodd" />
+                </svg>
+            </span>
+            <input 
+                type="search" 
+                wire:model.live.debounce.250ms="search" 
+                placeholder="Search..." 
+                class="w-full pl-10 text-black rounded-lg">
+        </div>
+        <a class="flex items-center justify-center px-5 py-1 font-bold text-center text-black transition-all duration-300 ease-in-out rounded-md cursor-pointer bg-button-blue hover:opacity-60" href="{{ route('add-request') }}" wire:navigate>
+            Add New Request
+        </a>
+    </div>
+    @if (session('status'))
+        <div 
+            x-data="{ show: true }"
+            x-init="setTimeout(() => show = false, 6000)" 
+            x-show="show"
+            class="mt-10 text-green-400"
+        >
+            {{ session('status') }}
+        </div>
+    @endif
+    
+    
+    <div class="w-full p-3 mt-10 mb-16 text-black bg-white rounded-lg lg:p-6">
+        <h1 class="font-bold lg:text-3xl">Client Requests</h1>
+        <table class="w-full mt-5 border-collapse">
+            <thead>
+                <tr class="border-b">
+                    <th class="px-3 font-thin text-left text-gray-500 whitespace-nowrap">Deliverable Request</th>
+                    <th class="hidden px-6 font-thin text-left text-gray-500 xl:table-cell whitespace-nowrap">Requested At</th>
+                    <th class="hidden px-6 font-thin text-left text-gray-500 sm:table-cell whitespace-nowrap">As Needed By</th>
+                    <th class="pl-6 font-thin text-left text-gray-500 whitespace-nowrap">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($userRequests as $request)
+                    <tr class="border-b">
+                        <td class="px-3 py-5">
+                            <p class="font-bold">{{ $request->title }}</p>
+                            <p class="italic text-gray-700 md:hidden">{{ $request->user->name }}</p>
+                            <p class="text-sm text-gray-500 sm:hidden">{{ date('D, F j, Y', strtotime($request->needed_at)) }}</p>
+                        </td>
+                        <td class="hidden px-6 py-5 sm:table-cell whitespace-nowrap">{{ date('D, F j, Y', strtotime($request->created_at)) }}</td>
+                        <td class="hidden px-6 py-5 sm:table-cell whitespace-nowrap">{{ date('D, F j, Y', strtotime($request->needed_at)) }}</td>
+                        <td class="py-5 pl-6 rounded-r-lg">
+                            <a href="{{ route('requests.view-request', ['client' => $request->user->id, 'clientRequest' => $request->id]) }}" wire:navigate class="px-5 py-1 font-bold text-black transition-all duration-300 ease-in-out rounded-md bg-button-blue hover:opacity-60">View</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <div class="mt-5">
+            {{ $userRequests->links() }}
+        </div>
+    </div>
+    <div class="w-full p-3 text-black bg-white rounded-lg lg:p-6">
+        <h1 class="font-bold lg:text-3xl">Deliverables</h1>
+        <table class="w-full mt-5 border-collapse">
+            <thead>
+                <tr class="border-b">
+                    <th class="px-3 font-thin text-left text-gray-500 whitespace-nowrap">Title</th>
+                    <th class="hidden px-6 font-thin text-left text-gray-500 sm:table-cell whitespace-nowrap">Status</th>
+                    <th class="hidden px-6 font-thin text-left text-gray-500 xl:table-cell whitespace-nowrap">Last Update</th>
+                    <th class="hidden px-6 font-thin text-left text-gray-500 xl:table-cell whitespace-nowrap">Updated By</th>
+                    <th class="pl-6 font-thin text-left text-gray-500 whitespace-nowrap">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($deliverables as $deliverable)
+                    <tr class="border-b">
+                        <td class="px-3 py-5">
+                            <p class="font-bold">{{ $deliverable->title }}</p>
+                            <p class="italic text-gray-700 md:hidden">Client Name A</p>
+                            <p class="text-sm text-gray-500 sm:hidden">{{ date('D, F j, Y') }}</p>
+                        </td>
+                        <td class="hidden px-6 py-5 sm:table-cell whitespace-nowrap">{{ $deliverable->status }}</td>
+                        <td class="hidden px-6 py-5 xl:table-cell whitespace-nowrap">{{ date('D, F j, Y', strtotime($deliverable->updated_at)) }}</td>
+                        <td class="hidden px-6 py-5 xl:table-cell">{{ $deliverable->updatedBy->name }}</td>
+                        <td class="py-5 pl-6 rounded-r-lg">
+                            <a href="{{ route('requests.view-deliverable', ['client' => $deliverable->user->id, 'clientRequest' => $deliverable->id]) }}" wire:navigate class="px-5 py-1 font-bold text-black transition-all duration-300 ease-in-out rounded-md bg-button-blue hover:opacity-60">View</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
 </div>
---
+++ resources/views/livewire/requests/admin/edit-request.blade.php
@@ -0,0 +1,158 @@
+<?php
+
+use App\Models\ClientRequest;
+use Livewire\Attributes\Validate;
+use Livewire\Volt\Component;
+use App\Models\User;
+use Livewire\WithFileUploads;
+
+
+new class extends Component {
+  use WithFileUploads;
+
+  public $client;
+  public $clientRequest;
+  public $role;
+  public $photo;
+  public $img_path = '';
+  #[Validate('required')] 
+  public $title = '';
+  #[Validate('required')] 
+  public $date_need = '';
+  #[Validate('required')] 
+  public $status = '';
+  public $remarks = '';
+
+  public function mount(User $client, ClientRequest $clientRequest) {
+    $this->client = $client;
+    $this->clientRequest = $clientRequest;
+    $this->title = $clientRequest->title;
+    $this->date_need = $clientRequest->needed_at;
+    $this->status = $clientRequest->status;
+    $this->remarks = $clientRequest->remarks;
+    $this->img_path = $clientRequest->img_path;
+  }
+
+  public function handleSave()
+    {
+        $this->validate();
+
+        $image = $this->photo;
+        
+        if ($image) {    
+            $uuid = substr(Str::uuid()->toString(), 0, 8);
+            $file_name = $uuid . '.' . $image->getClientOriginalExtension();
+            $this->img_path = url('images/client-request/' . $file_name);
+            $image->storePubliclyAs('images/client-request', $file_name, 'public');
+
+            $this->clientRequest->update([
+                'img_path' => $this->img_path
+            ]);
+        }
+
+        $user_id = $this->clientRequest->update([
+            'title' => $this->title,
+            'status' => $this->status,
+            'updated_by' => auth()->user()->id,
+            'needed_at' => $this->date_need,
+            'remarks' => $this->remarks,
+            'created_at' => date('Y-m-d H:i:s')
+        ]);
+
+        $this->reset(['title', 'date_need', 'remarks']);
+        
+        session()->flash('success', 'Request successfully updated.');
+        $this->redirect(url()->previous(), navigate: true);
+    }
+}; ?>
+
+<div class="py-12">
+	<form action="" wire:submit="handleSave">
+		<div class="mx-auto space-y-6">
+			<div class="p-4 bg-white shadow sm:p-8 sm:rounded-lg">
+			<h1 class="font-bold text-black lg:text-3xl">Deliverable Form</h1>
+			<div class="grid sm:grid-cols-2 sm:gap-x-8">
+				<div class="mt-5 space-y-2">
+					<label for="" class="block tracking-wider text-gray-600">Created By</label>
+					<input 
+						class="w-full text-black rounded-lg"
+						type="text"
+						value="{{ $client->name }}"
+						readonly
+					>
+				</div>
+				<div class="mt-5 space-y-2">
+					<label for="" class="block tracking-wider text-gray-600">Created At</label>
+					<input 
+						class="w-full text-black rounded-lg"
+						type="text"
+						value="{{ (new DateTime($clientRequest->created_at))->format('D, F j, Y h:i a') }}"
+						readonly
+					>
+				</div>
+				<div class="mt-5 space-y-2">
+				<label for="" class="block tracking-wider text-gray-600">Title</label>
+				<input 
+					class="w-full text-black rounded-lg"
+					type="text"
+					wire:model="title"
+				>
+				@error('title') <p class="text-red-500">{{ $message }}</p> @enderror
+			</div>
+			<div class="mt-5 space-y-2">
+				<label for="" class="block tracking-wider text-gray-600">Date Need</label>
+				<input 
+					class="w-full text-black rounded-lg"
+					type="date"
+					wire:model="date_need"
+				>
+				@error('date_need')<p class="text-red-500">{{ $message }}</p>@enderror
+			</div>
+			<div class="mt-5 space-y-2">
+				<label for="" class="block tracking-wider text-gray-600">Status</label>
+				<select 
+					class="w-full text-black rounded-lg"
+					wire:model.change="status"
+				>
+					<option value="" disabled selected>Select status</option>
+					@foreach (config('global.deliverable_statuses') as $deliverableStatus)
+						<option value="{{ $deliverableStatus }}">{{ $deliverableStatus }}</option>
+					@endforeach
+				</select>
+				@error('status')<p class="text-red-500">{{ $message }}</p>@enderror
+			</div>
+			<div class="mt-5 space-y-2">
+					@if ($photo) 
+					<img src="{{ $photo->temporaryUrl() }}" class="mb-5 rounded-lg shadow-md max-w-48">
+					@elseif($img_path)
+						<img src="{{ $img_path }}?{{ now()->timestamp }}" class="mb-5 rounded-lg shadow-md max-w-48">
+					@endif
+				<label for="" class="block tracking-wider text-gray-600">Upload Photo</label>
+				<input 
+					class="w-full max-w-lg"
+					type="file"
+					wire:model="photo"
+				>
+				@error('photo')<p class="text-red-500">{{ $message }}</p>@enderror
+			</div>
+			<div class="w-full mt-5 space-y-2 sm:col-span-2" wire:ignore>
+				<label for="" class="block tracking-wider text-gray-600">Remarks</label>
+				<trix-editor
+					class="text-black trix"
+					x-data
+					x-on:trix-change="$dispatch('input', event.target.value)"
+					x-ref="trix"
+					wire:model.debounce.60s="remarks"
+					wire:key="uniqueKey"
+				></trix-editor>
+				@error('remarks')<p class="mt-2 text-red-500">{{ $message }}</p>@enderror
+			</div>
+			</div>
+			<button 
+				class="px-4 py-2 mt-5 text-right text-white bg-blue-500 border rounded-lg hover:bg-blue-600"
+				type="Submit" 
+			>
+			Submit
+		</button>      
+	</form>
+</div>
