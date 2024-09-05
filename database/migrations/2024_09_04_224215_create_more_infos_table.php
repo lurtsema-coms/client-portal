febdfae6a33bb317bd5745b936260bd12955571d
Fil <filongunio@gmail.com>
2024-09-06 00:20:24 +0800

    create model and migration for more info

---
+++ database/migrations/2024_09_04_224215_create_more_infos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('more_infos', function (Blueprint $table) {
+            $table->id();
+            $table->string('client_type')->length(10);
+            $table->string('label')->length(100);
+            $table->smallInteger('order')->unsigned();
+            $table->smallInteger('created_by')->unsigned();
+            $table->smallInteger('updated_by')->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('more_infos');
+    }
+};
