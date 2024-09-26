24d5c74a034c4963ff63b11d1b4d53b796f70fb9
Fil <filongunio@gmail.com>
2024-09-27 02:08:37 +0800

    show data type on table of more info

--- resources/views/livewire/more-info.blade.php
+++ resources/views/livewire/more-info.blade.php
@@ -34,6 +34,7 @@ new class extends Component {
             <thead>
                 <tr class="border-b">
                     <th class="px-3 font-thin text-left text-gray-500 whitespace-nowrap">Label</th>
+                    <th class="hidden px-6 font-thin text-left text-gray-500 sm:table-cell whitespace-nowrap">Data Type</th>
                     <th class="hidden px-6 font-thin text-left text-gray-500 sm:table-cell whitespace-nowrap">Created By</th>
                     <th class="hidden px-6 font-thin text-left text-gray-500 sm:table-cell whitespace-nowrap">Created At</th>
                 </tr>
@@ -43,8 +44,10 @@ new class extends Component {
                     <tr class="border-b">
                         <td class="px-3 py-5">
                             <p class="font-bold">{{ $businessInfo->label }}</p>
+                            <p class="text-sm italic text-gray-500 sm:hidden">Data Type: {{ $businessInfo->data_type }}</p>
                             <p class="text-sm text-gray-500 sm:hidden">Created by {{ $businessInfo->user->name }} on {{ date('D, F j, Y h:i a', strtotime($businessInfo->created_at)) }}</p>
                         </td>
+                        <td class="hidden px-6 py-5 sm:table-cell whitespace-nowrap">{{ $businessInfo->data_type }}</td>
                         <td class="hidden px-6 py-5 sm:table-cell whitespace-nowrap">{{ $businessInfo->user->name }}</td>
                         <td class="hidden px-6 py-5 sm:table-cell whitespace-nowrap">{{ date('D, F j, Y h:i a', strtotime($businessInfo->created_at)) }}</td>
                     </tr>
@@ -70,6 +73,7 @@ new class extends Component {
             <thead>
                 <tr class="border-b">
                     <th class="px-3 font-thin text-left text-gray-500 whitespace-nowrap">Label</th>
+                    <th class="hidden px-6 font-thin text-left text-gray-500 sm:table-cell whitespace-nowrap">Data Type</th>
                     <th class="hidden px-6 font-thin text-left text-gray-500 sm:table-cell whitespace-nowrap">Created By</th>
                     <th class="hidden px-6 font-thin text-left text-gray-500 sm:table-cell whitespace-nowrap">Created At</th>
                 </tr>
@@ -79,8 +83,10 @@ new class extends Component {
                     <tr class="border-b">
                         <td class="px-3 py-5">
                             <p class="font-bold">{{ $politicalInfo->label }}</p>
+                            <p class="text-sm italic text-gray-500 sm:hidden">Data Type: {{ $politicalInfo->data_type }}</p>
                             <p class="text-sm text-gray-500 sm:hidden">Created by {{ $politicalInfo->user->name }} on {{ date('D, F j, Y h:i a', strtotime($politicalInfo->created_at)) }}</p>
                         </td>
+                        <td class="hidden px-6 py-5 sm:table-cell whitespace-nowrap">{{ $politicalInfo->data_type }}</td>
                         <td class="hidden px-6 py-5 sm:table-cell whitespace-nowrap">{{ $politicalInfo->user->name }}</td>
                         <td class="hidden px-6 py-5 sm:table-cell whitespace-nowrap">{{ date('D, F j, Y h:i a', strtotime($politicalInfo->created_at)) }}</td>
                     </tr>
