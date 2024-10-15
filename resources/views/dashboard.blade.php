e76fca3d3f0c99253baa2906ce8848038d1c2240
Fil <filongunio@gmail.com>
2024-10-16 00:48:14 +0800

    create page for dashboard of client

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,8 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
+<x-admin-layout>
+    <div class="flex flex-col items-center justify-start gap-5">
+        <div class="flex justify-center w-full">
+            <h1 class="mb-2 text-3xl font-bold text-center md:text-6xl md:mb-8 lg:text-left">Welcome to your Portal!</h1>
         </div>
+        this is the dashboard
     </div>
-</x-app-layout>
+</x-admin-layout>
