e905782707947f565b6f8fa5a02bbdaca4b444b2
Fil <filongunio@gmail.com>
2024-10-26 07:40:05 +0800

    show client name on view client page

--- resources/views/livewire/clients/view-client.blade.php
+++ resources/views/livewire/clients/view-client.blade.php
@@ -82,6 +82,7 @@ class extends Component {
 
 <div class="w-full mx-auto ">
     <x-header-title headingTitle="Client Deliverables" backButton="true" />
+    <p class="text-2xl italic text-gray-400">{{ $client->name }}</p>
     <div class="flex flex-col w-full gap-5 mt-5 lg:items-center lg:justify-between lg:flex-row">
         <div class="flex flex-col-reverse flex-1 gap-5 shrink-0 lg:flex-row">
             <a href="{{ route('requests.add-deliverable', $client->id) }}" class="max-w-60" wire:navigate>
