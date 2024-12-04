18e1855cb8b1110b97a5d699e6fb2aa973595613
Fil <filongunio@gmail.com>
2024-12-05 01:58:25 +0800

    add sub item feature in add deliverable page

--- resources/views/livewire/requests/admin/add-deliverable.blade.php
+++ resources/views/livewire/requests/admin/add-deliverable.blade.php
@@ -5,6 +5,7 @@ use Livewire\Attributes\Validate;
 use Livewire\Volt\Component;
 use App\Models\User;
 use Livewire\WithFileUploads;
+use Illuminate\Support\Str;
 use Livewire\Attributes\Layout;
 
 
@@ -16,16 +17,28 @@ class extends Component {
   public $role;
   public $photo;
   public $img_path = '';
-  #[Validate('required')] 
   public $title = '';
-  #[Validate('required')] 
   public $date_need = '';
-  #[Validate('required')] 
   public $status = '';
   public $remarks = '';
+  public $subItems = [];
 
   public function mount(User $client) {
     $this->client = $client;
+  }
+
+  public function addSubItem() {
+    $this->subItems[] = [
+      'title' => '',
+      'description' => '',
+      'link' => '',
+      'file' => '',
+    ];
+  }
+
+  public function removeSubItem($index) {
+    unset($this->subItems[$index]);
+    $this->subItems = array_values($this->subItems);
   }
 
   public function handleSave()
@@ -33,12 +46,26 @@ class extends Component {
         $this->validate();
 
         $file = $this->photo;
+		$subItems = [];
+		foreach ($this->subItems as $subItem) {
+			$subItemFile = $subItem['file'];
+			if ($subItemFile) {
+				$uuid = substr(Str::uuid()->toString(), 0, 8);
+				$file_name = $uuid . '.' . $subItemFile->getClientOriginalExtension();
+				$link = url('images/sub-items/' . $file_name);
+				$subItemFile->storePubliclyAs('images/sub-items', $file_name, 'public');
+				unset($subItem['file']);
+				$subItem['link'] = $link;
+			}
+			$subItems[] = $subItem;
+		}
         $clientRequest = ClientRequest::create([
             'title' => $this->title,
             'status' => $this->status,
             'user_id' => $this->client->id,
             'needed_at' => $this->date_need,
             'remarks' => $this->remarks,
+			'subitems' => sizeof($subItems) ? json_encode($subItems) : null,
             'updated_by' => auth()->user()->id,
         ]);
 
@@ -56,6 +83,26 @@ class extends Component {
         session()->flash('success', 'Deliverable added successfully.');
         $this->redirect(route('clients.view-client', $this->client->id), navigate: true);
     }
+
+	public function rules() {
+		$rules = [
+			'title' => 'required',
+			'date_need' => 'required|date',
+			'status' => 'required|in:'.implode(',',config('global.deliverable_statuses')),
+			'subItems.*.title' => 'required|min:3|max:255',
+			'subItems.*.description' => 'nullable',
+			'subItems.*.link' => 'nullable|url',
+			'subItems.*.file' => 'nullable|file|mimes:jpg,jpeg,png,svg',
+		];
+		return $rules;
+	}
+
+	public function messages(): array {
+		return [
+			'subItems.*.title.required' => 'This field is required.',
+			'subItems.*.file.mimes' => 'Invalid file type.'
+		];
+	}
 }; ?>
 
 <div class="flex flex-col items-stretch justify-start gap-5">
@@ -146,6 +193,54 @@ class extends Component {
         ></trix-editor>
           @error('remarks')<p class="mt-2 text-red-500">{{ $message }}</p>@enderror
       </div>
+
+      {{-- sub-items --}}
+      <div class="mt-5 space-y-2">
+        <label for="" class="block tracking-wider text-gray-600">Sub Items</label>
+        <div class="grid grid-cols-1 gap-4 xl:grid-cols-2">
+          @foreach ($subItems as $index => $subItem)
+            <div class="grid grid-cols-4 overflow-hidden text-sm text-black border border-gray-300 rounded-md">
+              <div class="flex items-center flex-grow-0 px-2 py-1 bg-white border-r border-gray-300">
+				<p class="text-gray-500">Title</p>	
+              </div>
+              <div class="flex flex-col items-start justify-center col-span-3 px-2 py-1 bg-white">
+                <input wire:model="subItems.{{ $index }}.title" type="text" class="w-full py-1 border-t-0 border-b border-l-0 border-r-0 focus:outline-0 focus:border-button-blue border-button-blue">
+				@error("subItems.$index.title") <p class="text-red-500">{{ $message }}</p> @enderror
+              </div>
+              <div class="flex items-center px-2 py-1 bg-white border-t border-r border-gray-300">
+                <p class="text-gray-500">Description </p>
+              </div>
+              <div class="flex flex-col items-start justify-center col-span-3 px-2 py-1 bg-white border-t">
+                <input wire:model="subItems.{{ $index }}.description" type="text" class="w-full py-1 border-t-0 border-b border-l-0 border-r-0 focus:outline-0 focus:border-button-blue border-button-blue">
+				@error("subItems.$index.description") <p class="text-red-500">{{ $message }}</p> @enderror
+              </div>
+              <div class="flex items-start px-2 py-1 bg-white border-t border-r border-gray-300">
+                <p class="text-gray-500">Attach Image</p>
+              </div>
+              <div class="flex flex-col items-start justify-center col-span-3 px-2 py-1 bg-white border-t">
+                <input accept=".jpg,.jpeg,.png,.svg" wire:model="subItems.{{ $index }}.file" type="file" class="w-full py-1 border-t-0 border-b border-l-0 border-r-0 focus:outline-0 focus:border-button-blue border-button-blue">
+				@error("subItems.$index.file") <p class="text-red-500">{{ $message }}</p> @enderror
+				<div class="w-full h-48">
+					@if (($subItem['file'] && in_array($subItem['file']->extension(), ['jpg', 'jpeg', 'png', 'bmp', 'gif', 'svg'])) ?? $subItem['link'])
+					<img src="{{ $subItem['file']->temporaryUrl() ?? $subItem['link'] }}" class="max-w-full mb-5 shadow max-h-48" alt="Attached Subitem Image">
+					@else
+					<p class="mt-3 text-sm text-gray-400">No image selected...</p>
+					@endif
+				</div>
+              </div>
+              <div class="flex items-center px-2 py-1 bg-white border-t border-r border-gray-300">
+                <p class="text-gray-500">Action</p>
+              </div>
+              <div class="flex items-center col-span-3 px-2 py-1 bg-white border-t">
+                <button type="button" wire:click="removeSubItem('{{ $index }}')" class="px-2 py-1 text-white bg-red-600 rounded-md hover:opacity-50">Delete</button>
+              </div>
+            </div>
+
+          @endforeach
+        </div>
+        <button wire:click="addSubItem" type="button" class="px-2 py-1 rounded-md bg-button-blue hover:opacity-50">Add Item</button>
+      </div>
+      <hr class="mt-10">
       <button 
         class="px-4 py-2 mt-5 text-right text-white bg-blue-500 border rounded-lg hover:bg-blue-600"
         type="Submit" 
