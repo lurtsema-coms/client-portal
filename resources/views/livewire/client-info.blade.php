f39665257065b0beb61465f52ab0482db5c452b8
Fil <filongunio@gmail.com>
2024-12-12 03:06:58 +0800

    remove the word "personal"

--- resources/views/livewire/client-info.blade.php
+++ resources/views/livewire/client-info.blade.php
@@ -31,7 +31,7 @@ new class extends Component {
             <div class="flex flex-col">
                 <h3 class="text-2xl font-bold">{{ ucwords($client->client_type) }} Client</h3>
                 <div class="flex flex-col mt-3 lg:flex-row gap-x-10 lg:gap-x-40 xl:gap-x-52">
-                    <p class=""><span class="font-semibold">Personal Email: </span>{{ $client->email }}</p>
+                    <p class=""><span class="font-semibold">Email: </span>{{ $client->email }}</p>
                     <p class=""><span class="font-semibold">Cell Number: </span>{{ $client->company_cell_number }}</p>
                 </div>
                 <p class=""><span class="font-semibold">Address: </span>{{ $client->company_address }}</p>
